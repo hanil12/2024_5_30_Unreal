2fcb593e1e9d2a158d88bb75994882f3603ce0c0
hanil12 <ekdrhaod@gmail.com>
2024-07-16 15:27:10 +0900

    Lis
    과제
    1. 완전탐색 - 소수찾기
    2. 해쉬 - 폰켓몬

---
+++ Algorithm/WINAPI_Algorithm/Algorithm/DP(2).cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <Windows.h>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+// LIS (Longest Increasing Subsequence)
+// 최장 증가 부분 수열
+
+vector<int> v = {5,6,7,1,2,3,4};
+// 증가 부분 수열 : 점점 증가되는 부분 수열
+// 최장 증가 부분 수열 : 증가 부분 수열 중에 가장 긴 것
+
+// Lis(4) : 1
+// Lis(3) ...v[3] <= v[next] -> : 2
+// Lis(2) ...v[2] <= v[3] -> 3  / v[2] <= v[4] -> 2
+// Lis(1) ...Lis(2) + 1 / Lis(3) + 1 / Lis(4) + 1
+
+vector<int> cache;
+
+int Lis(int pos)
+{
+// 기저사항
+	if(pos == v.size() - 1)
+		return 1;
+
+// 메모이제이션
+	int& ref = cache[pos];
+	if(ref != -1)
+		return ref;
+
+	ref = 1;
+	for (int next = pos + 1; next < v.size(); next++)
+	{
+		if (v[pos] <= v[next])
+		{
+			ref = max(ref, Lis(next) + 1);
+		}
+	}
+
+	return ref;
+}
+
+int main()
+{
+	cache = vector<int>(v.size(), -1);
+	int lis = 1;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		lis = max(lis, Lis(i));
+	}
+
+	cout << lis << endl;
+
+	return 0;
+}
