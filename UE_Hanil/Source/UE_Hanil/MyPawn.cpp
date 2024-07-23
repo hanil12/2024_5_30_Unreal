e448f9607b77e720ccf7dfa14f56d4ab1d0164e0
hanil12 <ekdrhaod@gmail.com>
2024-07-23 14:25:08 +0900

    PawnControl 2

--- UE_Hanil/Source/UE_Hanil/MyPawn.cpp
+++ UE_Hanil/Source/UE_Hanil/MyPawn.cpp
@@ -2,6 +2,10 @@
 
 
 #include "MyPawn.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 // Sets default values
 AMyPawn::AMyPawn()
@@ -9,6 +13,16 @@ AMyPawn::AMyPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	_sm = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_moveMent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
+	RootComponent = _sm;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> mesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Props/SM_Chair.SM_Chair'"));
+
+	if (mesh.Succeeded())
+	{
+		_sm->SetStaticMesh(mesh.Object);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -22,9 +36,6 @@ void AMyPawn::BeginPlay()
 void AMyPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-
-	
 }
 
 // Called to bind functionality to input
@@ -33,19 +44,60 @@ void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	//  대리자 : 함수 포인터, 함수 객체 => CallAble, 콜백함수
-	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AMyPawn::UpDown);
-	PlayerInputComponent->BindAxis(TEXT("RightLeft"), this, &AMyPawn::RightLeft);
+	//PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AMyPawn::UpDown);
+	//PlayerInputComponent->BindAxis(TEXT("RightLeft"), this, &AMyPawn::RightLeft);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Moving
+		EnhancedInputComponent->BindAction(_moveAction, ETriggerEvent::Triggered, this, &AMyPawn::Move);
+
+		// Looking
+		EnhancedInputComponent->BindAction(_lookAction, ETriggerEvent::Triggered, this, &AMyPawn::Look);
+	}
+}
+
+void AMyPawn::Move(const FInputActionValue& value)
+{
+	FVector2D MovementVector = value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AMyPawn::Look(const FInputActionValue& value)
+{
+	FVector2D LookAxisVector = value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
 }
 
 void AMyPawn::UpDown(float value)
 {
+	FVector forward = GetActorForwardVector();
+	AddMovementInput(forward, value);
+	
+
+
 	if (abs(value) >= 0.01)
-		UE_LOG(LogTemp, Log, TEXT("UpDown : %f"), value);
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *forward.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("%f"), value);
+	}
 }
 
 void AMyPawn::RightLeft(float value)
 {
 	if(abs(value) >= 0.01)
-		UE_LOG(LogTemp, Log, TEXT("RightLeft : %f"), value);
+	{
+		FVector rightLeft = GetActorRightVector();
+		AddMovementInput(rightLeft, value);
+	}
 }
 
--- UE_Hanil/Source/UE_Hanil/MyPawn.h
+++ UE_Hanil/Source/UE_Hanil/MyPawn.h
@@ -6,10 +6,24 @@
 #include "GameFramework/Pawn.h"
 #include "MyPawn.generated.h"
 
+class UInputComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
 UCLASS()
 class UE_HANIL_API AMyPawn : public APawn
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* _moveAction;
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* _sm;
+
+	UPROPERTY(VisibleAnywhere)
+	class UFloatingPawnMovement* _moveMent;
 
 public:
 	// Sets default values for this pawn's properties
@@ -20,11 +34,19 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* _lookAction;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+
+	void Move(const FInputActionValue& value);
+	void Look(const FInputActionValue& value);
+
 
 	void UpDown(float value);
 	void RightLeft(float value);
