af8b83b3b16ce8242ad32319c646ea6ef5d57ba4
hanil12 <ekdrhaod@gmail.com>
2024-07-24 09:17:26 +0900

    PlayerController

---
+++ UE_Hanil/Source/UE_Hanil/MyPlayerController.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UEnhancedInputLocalPlayerSubsystem* subSystem = 
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if (subSystem)
+	{
+		subSystem->AddMappingContext(_inputMappingContext,0);
+	}
+}
---
+++ UE_Hanil/Source/UE_Hanil/MyPlayerController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_HANIL_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	class UInputMappingContext* _inputMappingContext;
+
+protected:
+
+	virtual void BeginPlay() override;
+};
