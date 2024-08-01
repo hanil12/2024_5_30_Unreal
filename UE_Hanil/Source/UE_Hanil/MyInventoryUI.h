232aff92b1effc8f55f1a686ad393e3854e9c1b3
hanil12 <ekdrhaod@gmail.com>
2024-08-01 14:40:20 +0900

    Inventory(0)

---
+++ UE_Hanil/Source/UE_Hanil/MyInventoryUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyInventoryUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_HANIL_API UMyInventoryUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
