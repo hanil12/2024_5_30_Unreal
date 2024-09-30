8d0d93e4d4d928cf553b5727e9170a2e6f7ad2ca
hanil12 <ekdrhaod@gmail.com>
2024-09-30 14:15:22 +0900

    Condition Variable
    - Event Lock 개선

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -6,24 +6,40 @@
 // Lock 구현
 // 1. Spin Lock(Busy Waiting)
 // 2. Sleep 기반 Lock
-// 3. Event 기반 Lock
+// 3. Event 기반 Lock ...=> CV(conditon variable)
+//
+// 3... Linux에서도,,?
+// => cv(condition_variable)
+// ... 조건을 걸고 조건이 참일 때만 락을 걸어잠그고 실행
+// => 멀티쓰레드 환경에서 Sleep을 시키지 않으면 올바르게 동작하지 않을 수도 있다.
+// ... 동시성이 생길 때 Produce를 더 할 수도 있다.
+
+// 디자인 패턴
+// Produce And Consumer 패턴... Queue
 
 queue<int32> q;
 mutex m;
-HANDLE handle;
+//HANDLE handle;
+condition_variable cv;
 
 void Producer()
 {
 	while (true)
 	{
+		// P_A_C P...
+		// 1. Lock을 잡고 (lock(m))
+		// 2. 공유 변수 값을 수정 (q.push(100))
+		// 3. Lock을 풀고
+		// 4. 조건변수를 통해 다른 쓰레드에게 통지
+
 		{
-			unique_lock<std::mutex> lock(m);
-			q.push(100);
-		}
+			unique_lock<std::mutex> lock(m); // 1
 
-		::SetEvent(handle);
+			q.push(100); // 2
+		} // 3
 
-		this_thread::sleep_for(10000ms);
+		// wait 중인 Thread가 있으면 딱 1개를 깨운다.
+		cv.notify_one(); // 4
 	}
 }
 
@@ -31,15 +47,21 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
+		// P_A_C : Consumer
+		// 1. Lock을 잡고
+		// 2. 조건확인
+		// - 만족했다 => 이어서 코드진행
+		// - 불만족 => 'Lock을 풀어주고' 대기상태
 
-		unique_lock<std::mutex> lock(m);
+		unique_lock<std::mutex> lock(m); // 1
 
-		if (q.empty() == false)
+		cv.wait(lock, []()->bool { return q.empty() == false; }); // 2
+
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			//cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
@@ -48,7 +70,7 @@ int main()
 {
 						// 보안속성, 매뉴얼 리셋 관련, 초기상태
 						//			Manual : 수동의
-	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	//handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
 
 	std::thread t1(Producer);
 	std::thread t2(Consumer);
@@ -56,7 +78,7 @@ int main()
 	t1.join();
 	t2.join();
 
-	::CloseHandle(handle);
+	//::CloseHandle(handle);
 
 	return 0;
 }
