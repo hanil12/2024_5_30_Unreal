d1413be97cc8afaa2cf92c1254f8544080ba09e3
hanil12 <ekdrhaod@gmail.com>
2024-10-02 13:38:16 +0900

    Cache

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -3,93 +3,54 @@
 #include "AccountManager.h"
 #include "UserManager.h"
 
-int64 Caculate()
-{
-	int64 sum = 0;
+// 캐시
+// - CPU에 있는 임시 저장공간
+// - 코어 마다 있다.(L1, L2), 아닌 것도 있다(L3)
+// 
+// - 캐시 철학
+// -- Temporal Locality : 시간적 지역 -> '방금'본 메모리에서 또 다시 접근할 일이 생길 거 같다.
+// -- Spatial Locality : 공간적 지역 -> 봤던 메모리 '주변'에 또 접근할 일이 생길 거 같다.
+// => 메모리 탐색을 최소화 시키려는 전략
 
-	for (int i = 0; i < 100'0000; i++)
-	{
-		sum++;
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		int temp = 0; // 동기로 실행됬을 때는 temp가 먼저 잡히고
-					  // 비동기로 실행됬을 때는 예측할 수 없다.
-	}
-
-	return sum;
-}
-
-void PromiseWorker(std::promise<string>&& promise)
-{
-	promise.set_value("Set Message");
-}
-
-void TaskWorker(std::packaged_task<int64(void)>&& task)
-{
-	task();
-}
+int32 buffer[10000][10000];
 
 int main()
 {
-	// 동기방식(synchronous)
-	int64 sum = Caculate();
+	memset(buffer, 0, sizeof(buffer));
 
-	// future : 미래
 	{
-		// 비동기방식(asynchronous)
-		// std::launch::deferred => 지연해서 실행시켜주세요.
-		// std::launch::async => 쓰레드를 하나 따로 만들어서 실행
-		std::future<int64> future = std::async(std::launch::async, Caculate);
+		uint32 start = GetTickCount64();
 
-		// TODO .... => future가 끝나던지 끝나지 않던지 간에 이 내용들이 실행
-		int t = 1;
-
-		int64 sum = future.get(); // get : 함수가 끝날 떄 까지 기달려서 리턴값
-
-		cout << sum << endl;
-	}
-
-	// promise 
-	// 미래(future)에 결과물을 반환할 것이라는 약속(promise)을 해줘 -> 계약서
-	{
-		std::promise<string> promise;
-		std::future<string> future = promise.get_future(); // future에 반환하겠다는 약속
-
-		thread t(PromiseWorker, std::move(promise)); // promise한테 일 시키기
-
-		// future.wait(); // future가 잘 세팅되어있는지 확인... 끝날 때까지 대기
-		std::future_status status = future.wait_for(10ms); // 0.01초동안 대기하면서 future가 끝났는지 확인.
-		if (status == std::future_status::ready)
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
 		{
-			// TODO : future가 세팅이 잘되어있다. => 즉시실행 가능
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[i][j];
+			}
 		}
 
-		string value = future.get(); // 비동기 방식으로 future에 값이 잘 세팅이 되어있는지 확인하고 반환.
+		uint32 end = GetTickCount64();
 
-		cout << value << endl;
-
-		t.join();
+		cout << "Elipsed Time : " << end - start << endl;
 	}
 
-	// packaged_task : 일감을 만들어서 넘겨주는 방법
 	{
-		std::packaged_task<int64()> task(Caculate);
-		std::future<int64> future = task.get_future();
+		uint32 start = GetTickCount64();
 
-		thread t(TaskWorker, std::move(task));
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+		{
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[j][i];
+			}
+		}
 
-		int64 sum = future.get();
+		uint32 end = GetTickCount64();
 
-		cout << sum << endl;
-
-		t.join();
+		cout << "Elipsed Time : " << end - start << endl;
 	}
-
-	// Cacluate하는데 Event방식, SpinLock 방식처럼 Lock,thread만들고 하는 작업들을 최소화
-	// ... 작은 작업들을 비동기 방식으로 할 때 자주 쓴다.
-	// => future
 
 	return 0;
 }
