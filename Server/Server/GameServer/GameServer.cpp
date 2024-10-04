188a5debd3e23ca967aa705642b01cbe365debd3
hanil12 <ekdrhaod@gmail.com>
2024-10-04 15:39:00 +0900

    LockBased stack, queue

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -3,43 +3,47 @@
 #include "AccountManager.h"
 #include "UserManager.h"
 
-// TLS 영역이란..
-// 각 쓰레드마다 갖고 있는 고유 메모리 저장공간
+// Lock based Stack
+// Lock based Queue
 
-// 거실에 일감이 있다.
-// -> 거실에 있는 일감을 4명이서 분배해서 일을 하고 싶다.
-// -> 4명이서 거실에 다 같이 일을 하려고 한다
-// -> 일감을 조금씩 떼어서 각자 자기방에 들어가서 일을 해놓고
-//    다시 거실에 갖다놓는다면?
+LockBasedStack<int32> s;
+LockBasedQueue<int32> q;
 
-// TLS (Thread Local Storage)
-
-thread_local int32 L_threadId; // TLS 영역
-
-void MySetThreadId(int32 id)
+void Push()
 {
-	L_threadId = id;
-
 	while (true)
 	{
-		cout << "Hi Im thread : " << L_threadId << endl;
-		this_thread::sleep_for(1000ms);
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(100ms);
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+		
+		//s.WaitPop(data);
+
+		if (q.TryPop(data))
+		{
+			cout << data << endl;
+		}
 	}
 }
 
 int main()
 {
-	vector<thread> threads;
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
 
-	for (int i = 0; i < 10; i++)
-	{
-		threads.push_back(thread(MySetThreadId,(i + 1)));
-	}
-
-	for (auto& t : threads)
-	{
-		t.join();
-	}
+	t1.join();
+	t2.join();
+	t3.join();
 
 	return 0;
 }
