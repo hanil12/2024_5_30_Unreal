babb736f50e24300a276b3f98a1b98efc0289009
hanil12 <ekdrhaod@gmail.com>
2024-10-14 13:43:20 +0900

    스마트포인터 복습

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -7,10 +7,19 @@
 #include "Lock.h"
 #include "RefCounting.h"
 
+using PlayerRef = shared_ptr<class Player>;
+using InventoryRef = shared_ptr<class Inventory>;
 
-class Player : public RefCountable
+class Player
 {
 public:
+	Player() : _hp(0), _atk(0) {}
+
+	~Player()
+	{
+
+	}
+
 	void Attack()
 	{
 		if (_target != nullptr)
@@ -25,41 +34,56 @@ public:
 	}
 
 public:
-	Player* _target;
+	PlayerRef _target;
+	weak_ptr<Inventory> _inven;
 
 	int _hp;
 	int _atk;
 };
 
-int main()
+class Inventory
 {
-	CoreGlobal::Create();
+public:
+	Inventory(PlayerRef p) : _owner(p) {} //: _owner(**p) {} // 스마트포인터의 참조카운트를 올리지 않으면서 참조
 
-	Player* p1 = new Player();
-	p1->_hp = 10000;
-	p1->_atk = 15;
-
-	Player* p2 = new Player();
-	p2->_hp = 20000;
-	p2->_atk = 15;
-	p2->_target = p1;
-	p1->AddRef();
-
-	TSharedPtr<Player> p3 = TSharedPtr<Player>(new Player());
-
-	while (true)
+	void OpenInven()
 	{
-		if (p1 != nullptr)
+		if (_owner.expired() == false)
 		{
-			p2->Attack();
-
-			if (p1->IsDead())
-			{
-				p1->ReleaseRef(); // t1 t2
-				break;
-			}
+			auto ptr = _owner.lock();
+			if(ptr != nullptr)
+				cout << ptr->_hp << ptr->_atk << endl;
 		}
 	}
+public:
+	// Player& _owner;
+	weak_ptr<Player> _owner;
+};
+
+int main()
+{
+	// shared_ptr
+	// - 참조 카운팅
+	// - 순환 참조 문제
+
+	// weak_ptr
+	// - Weak Counting을 한다.
+	// - 순환 참조 문제를 해결하기 위해 쓰이는 ptr
+	// - 생성주기에 관여하지 않는다.
+
+	
+	CoreGlobal::Create();
+
+	PlayerRef p = make_shared<Player>();
+	InventoryRef inven = make_shared<Inventory>(p);
+	p->_inven = inven;
+	// => 순환참조
+	
+	inven->OpenInven();
+
+	//p->_inven = nullptr;
+	//inven->_owner = nullptr;
+	// => 순환참조 고리 끊기
 
 	CoreGlobal::Delete();
 
