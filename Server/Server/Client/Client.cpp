054e2ff989768e98d73d67407ca4774f3ad36feb
hanil12 <ekdrhaod@gmail.com>
2024-10-28 13:56:02 +0900

    Socket Option

--- Server/Server/Client/Client.cpp
+++ Server/Server/Client/Client.cpp
@@ -17,7 +17,7 @@ int main()
 	if(::WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
 		return 0;
 
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
 		int32 errCode = ::WSAGetLastError();
@@ -31,43 +31,8 @@ int main()
 	::inet_pton(AF_INET,"127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777);
 
-	while (true)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			char sendBuffer[100] = "Hello World!!";
-			int32 sendCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-			if (sendCode == SOCKET_ERROR)
-			{
-				int32 errorCode = ::WSAGetLastError();
-				cout << "Send ErrorCode : " << errorCode << endl;
-				return 0;
-			}
-		}
 
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 addrLen = sizeof(recvAddr);
 
-		char recvBuffer[1000];
-
-		// UDP
-		// 1:1 연결이 아니라, 1: 다 연결 가능
-		// listener가 없고 SeverSocket으로 나한테 정보를 보낸 것을 Recv한다.
-
-		int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr, &addrLen);
-		if (recvLen <= 0)
-		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "Recv ErrorCode " << endl;
-			return 0;
-		}
-
-		cout << "Recv Data : " << recvBuffer << endl;
-		cout << "Recv Len : " << recvLen;
-
-		this_thread::sleep_for(1s);
-	}
 
 	::closesocket(clientSocket);
 	::WSACleanup();
--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -12,7 +12,7 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (serverSocket == INVALID_SOCKET)
 	{
 		int32 errCode = ::WSAGetLastError();
@@ -26,49 +26,66 @@ int main()
 	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	serverAddr.sin_port = ::htons(7777);
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	// Socket level
+	// 옵션을 해석, 처리하는 주체
+	// 소켓 코드-> SOL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP -> IPPROTO_TCP
+
+	// Socket 옵션
+	// SO_KEEPALIVE => 주기적으로 연결이 끊겼는지 확인하는 옵션
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)(&enable), sizeof(enable));
+
+	// 소켓 만들기
+	// => 운영체제 ... Socket 번호
+	// => 운영체제 ... SendBuff, RecvBuff
+
+	// 우리가 하던 거
+	// sendBuff를 stack이나 heap에 사용자정의로 만든다.
+	char arr[100] = "temp";
+	// send(serverSocket, arr, 100, nullptr); => 사용자가 만든 sendBuffer를 운영체제가 만들어놓은 sendBuffer에 복사
+
+	// 소켓을 닫을려고함
+	// -> Send("~~")
+	// -> SendBuff에 복사
+	// -> Socket이 닫힘
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)(&linger), sizeof(linger));
+
+	// SendBuff의 크기 가져오기
+	int32 sendBuffSize;
+	int32 optionLen = sizeof(sendBuffSize);
+
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)(&sendBuffSize), &optionLen);
+	cout <<"SendBuffer Size : " << sendBuffSize << endl;
+
+	// recvBuff의 크기 가져오기
+	int32 recvBuffSize;
+	optionLen = sizeof(recvBuffSize);
+
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)(&recvBuffSize), &optionLen);
+	cout << "SendBuffer Size : " << recvBuffSize << endl;
+
+	// SO_REUSEADDR
+	// IP 주소 및 포트번호 재사용
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout <<"Bind ErrorCode : " << errCode << endl;
-		return 0;
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)(&enable), sizeof(enable));
 	}
 
-	// UDP
-	// ServerSocket.
-	// - 여러명의 socket들이 serverSocket한테 직접 정보를 전달
-	while (true)
+	// 네이글 알고리즘
+	// 작은 패킷들을 한번에 모아서 보내는 알고리즘
+	// 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단점 : 반응 시간 손해
+	// => 네이글 알고리즘이 처리하기 전에 프로그래머(우리)가 알아서 패킷 설계를 하고 보낼 거기 때문에
 	{
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int32 addrLen = sizeof(clientAddr);
-
-		this_thread::sleep_for(1s);
-
-		char recvBuffer[1000];
-
-		// UDP
-		// 1:1 연결이 아니라, 1: 다 연결 가능
-		// listener가 없고 SeverSocket으로 나한테 정보를 보낸 것을 Recv한다.
-		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&clientAddr, &addrLen);
-		if (recvLen <= 0)
-		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "Recv ErrorCode " << endl;
-			return 0;
-		}
-
-		cout << "Recv Data : " << recvBuffer << endl;
-		cout << "Recv Len : " << recvLen;
-
-		char sendBuffer[100] = "Hello Client I'm Server";
-		int32 sendCode = ::sendto(serverSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&clientAddr, sizeof(clientAddr));
-		if (sendCode == SOCKET_ERROR)
-		{
-			int32 errorCode = ::WSAGetLastError();
-			cout << "Send ErrorCode : " << errorCode << endl;
-			return 0;
-		}
+		bool enable = false;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)(&enable), sizeof(enable));
 	}
+
 	::WSACleanup();
 
 	return 0;
