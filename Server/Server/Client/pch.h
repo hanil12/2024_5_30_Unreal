ab6ebd27206fe06cb8899dbd179d07f28ca3df75
hanil12 <ekdrhaod@gmail.com>
2024-12-06 15:48:54 +0900

    ProtoBuf

--- Server/Server/Client/pch.h
+++ Server/Server/Client/pch.h
@@ -3,9 +3,11 @@
 #define WIN32_LEAN_AND_MEAN
 
 #ifdef _DEBUG
-#pragma comment(lib,"Debug\\ServerCore.lib")
+#pragma comment(lib,"ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib,"Protobuf\\Debug\\TODO.lib")
 #else
-#pragma comment(lib,"Release\\ServerCore.lib")
+#pragma comment(lib,"ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib,"Protobuf\\Release\\TODO.lib")
 #endif
 
 
--- Server/Server/GameServer/pch.h
+++ Server/Server/GameServer/pch.h
@@ -3,9 +3,11 @@
 #define WIN32_LEAN_AND_MEAN
 
 #ifdef _DEBUG
-#pragma comment(lib,"Debug\\ServerCore.lib")
+#pragma comment(lib,"ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib,"Protobuf\\Debug\\libprotobufd.lib")
 #else
-#pragma comment(lib,"Release\\ServerCore.lib")
+#pragma comment(lib,"ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib,"Protobuf\\Release\\libprotobuf.lib")
 #endif
 
 
